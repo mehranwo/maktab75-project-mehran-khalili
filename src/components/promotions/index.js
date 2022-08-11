e273a197f61d53f15e6720003a0af02cac19b9b1
mehran <me.dr7725@gmail.com>
2022-08-12 01:22:09 +0430

    feat: create src/components/promotions/index.js

---
+++ src/components/promotions/index.js
@@ -0,0 +1,43 @@
+import { Box, Slide } from "@mui/material";
+import { useEffect } from "react";
+import { useState } from "react";
+import { MessageText, PromotionsContainer } from "../../styles/promotions";
+
+const messages = [
+  "20درصد تخفیف شب عید ",
+  "جشنواره فروش عید تا عید ",
+];
+
+export default function Promotions() {
+  const [messageIndex, setMessageIndex] = useState(0);
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+
+    const intervalId = setInterval(() => {
+      setMessageIndex((i) => (i + 1) % messages.length);
+
+      setShow(true);
+
+      setTimeout(() => {
+        setShow(false);
+      }, 3000);
+    }, 4000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return (
+    <PromotionsContainer>
+      <Slide direction={show ? "left" : "right"} in={show}>
+        <Box display={"flex"} justifyContent="center" alignItems={"center"}>
+          <MessageText>{messages[messageIndex]}</MessageText>
+        </Box>
+      </Slide>
+    </PromotionsContainer>
+  );
+}

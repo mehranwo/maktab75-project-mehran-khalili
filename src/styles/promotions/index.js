67abafa2c613c0bdbe54315330892ad5714fa873
mehran <me.dr7725@gmail.com>
2022-08-12 01:22:40 +0430

    feat: create src/styles/promotions/index.js

---
+++ src/styles/promotions/index.js
@@ -0,0 +1,26 @@
+import { Box, Typography } from "@mui/material";
+import {styled} from "@mui/material/styles"
+import { fontSize } from "@mui/system";
+import { Colors } from "../theme";
+
+
+export const PromotionsContainer = styled(Box)(({theme})=>({
+    [theme.breakpoints.up('md')]:{
+        padding:"0px 40px"
+    },
+    marginTop:"15px",
+    display:'flex',
+    justifyContent:"center",
+    alignContent:"center",
+    padding:'0px 20px',
+    overflow:'hidden',
+    background:Colors.primary
+}))
+
+export const MessageText = styled(Typography)(({theme})=>({
+    [theme.breakpoints.up('md')]:{
+        fontSize:"3rem",
+    },
+    color:Colors.white,
+    fontSize:"1.5rem"
+})) 

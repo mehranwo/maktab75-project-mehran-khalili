47226e6a84d171facc972fd0627441a41c52903f
mehran <me.dr7725@gmail.com>
2022-08-12 01:14:19 +0430

    creat project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { ThemeProvider } from "@mui/system";
+import { Button, Container } from "@mui/material";
+import { useEffect } from "react";
+import theme from "./styles/theme";
+import Appbar from "./components/appbar";
+import { BrowserRouter} from "react-router-dom";
+import HandleRouter from "./Routes/HandleRouter";
+import Banner from "./components/banner";
+import Promotions from "./components/promotions";
+import { ManagmentProducts } from "./pages/managment";
+import DataGridDemo from "./components/test";
+import BasicTable from './components/table/table'
+import RTL from "./rtl";
+
+function App() {
+  useEffect(() => {
+    document.title = "mui project";
+  }, []);
+
+  return (
+    // <BrowserRouter>
+    //   <HandleRouter/>
+    // </BrowserRouter>
+    <>
+    <RTL>
+      <BasicTable/>
+    </RTL>
+    </>
+  );
+}
+
+export default App;

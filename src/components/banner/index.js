786d8549cd93ae3c62a8765855e88bb080414637
mehran <me.dr7725@gmail.com>
2022-08-12 01:21:30 +0430

    feat: create src/components/banner/index.js

---
+++ src/components/banner/index.js
@@ -0,0 +1,77 @@
+import { Typography, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import {
+  BannerContainer,
+  BannerContent,
+  BannerTitle,
+  BannerDescription,
+} from "../../styles/banner";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function Banner() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <BannerContainer src="../server/uploads/a82cf92477ca81301768f28a546d2183.jpg">
+            <BannerContent>
+              <Typography variant="h6">Huge Collection</Typography>
+              <BannerTitle variant="h2">New Bags</BannerTitle>
+              <BannerDescription variant="subtitle">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </BannerDescription>
+            </BannerContent>
+          </BannerContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+        <BannerContainer src="/images/banner/floral.jpg">
+            <BannerContent>
+              <Typography variant="h6">Huge Collection</Typography>
+              <BannerTitle variant="h2">New Bags</BannerTitle>
+              <BannerDescription variant="subtitle">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </BannerDescription>
+            </BannerContent>
+          </BannerContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+        <BannerContainer src="/images/banner/holy-guacamole.jpg">
+            <BannerContent>
+              <Typography variant="h6">Huge Collection</Typography>
+              <BannerTitle variant="h2">New Bags</BannerTitle>
+              <BannerDescription variant="subtitle">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </BannerDescription>
+            </BannerContent>
+          </BannerContainer>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}

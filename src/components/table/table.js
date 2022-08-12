d0603a8bfbab2b8a1e006992d4435f5b702e7918
mehran <me.dr7725@gmail.com>
2022-08-12 12:28:27 +0430

    update table.js

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,40 +1,115 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import { Typography } from "@mui/material";
 
-export default function BasicTable({rows}) {
+export default function BasicTable({ rows, columns }) {
+  const englishHeader = Object.keys(columns);
+  const persionHeader = Object.values(columns);
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer sx={{ minWidth: 650 }} component={Paper}>
+      <Table aria-label="simple table" size="small">
         <TableHead>
           <TableRow>
-            <TableCell>تصویر</TableCell>
-            <TableCell align="right">نام</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            {persionHeader &&
+              persionHeader.map((cell) => (
+                <TableCell align="center">{cell}</TableCell>
+              ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
+          {rows
+            ? rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  {englishHeader.includes("src") && (
+                    <TableCell
+                      align="center"
+                      sx={{ width: "120px", height: "150px" }}
+                    >
+                      <img
+                        src={`http://localhost:3003/files/${row.src[0]}`}
+                        width={"100%"}
+                        height={"100%"}
+                      />
+                    </TableCell>
+                  )}
+
+                  {englishHeader.includes("name") && (
+                    <TableCell align="center">
+                      <Typography>{row.name}</Typography>
+                    </TableCell>
+                  )}
+                  {englishHeader.includes("sum") && (
+                    <TableCell align="center">
+                      <Typography>{row.sum}</Typography>
+                    </TableCell>
+                  )}
+                  {englishHeader.includes("time") && (
+                    <TableCell align="center">
+                      <Typography>{row.time}</Typography>
+                    </TableCell>
+                  )}
+                  {englishHeader.includes("checkOrder") && (
+                    <TableCell align="center">
+                      <Button
+                        color="info"
+                        variant="outlined"
+                      >
+                        بررسی سفارش
+                      </Button>
+                    </TableCell>
+                  )}
+                  {englishHeader.includes("productName") && (
+                    <TableCell align="center">
+                      <Typography>{row.productName}</Typography>
+                    </TableCell>
+                  )}
+                  {englishHeader.includes("brand") && (
+                    <TableCell align="center">{row.brand}</TableCell>
+                  )}
+                  {englishHeader.includes("price") && (
+                    <TableCell align="center">{row.price}</TableCell>
+                  )}
+                  {englishHeader.includes("stock") && (
+                    <TableCell align="center">{row.stock}</TableCell>
+                  )}
+                  {englishHeader.includes("delete") && (
+                    <TableCell align="center">
+                      <Button
+                        color="error"
+                        startIcon={<DeleteRoundedIcon />}
+                        variant="outlined"
+                      >
+                        حذف
+                      </Button>
+                    </TableCell>
+                  )}
+                  {englishHeader.includes("edit") && (
+                    <TableCell align="center">
+                      <Button
+                        color="info"
+                        startIcon={<EditRoundedIcon />}
+                        variant="outlined"
+                      >
+                        ویرایش
+                      </Button>
+                    </TableCell>
+                  )}
+                </TableRow>
+              ))
+            : ""}
         </TableBody>
       </Table>
     </TableContainer>

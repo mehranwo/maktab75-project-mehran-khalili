2f1b156c0456dac689c14f5a4a3543496a764ed6
mehran <me.dr7725@gmail.com>
2022-08-12 12:28:58 +0430

    update src/services/index.jsx

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -3,7 +3,7 @@ import  BASE_URL  from "../configs/variables.config";
 
 class HttpService {
   constructor() {
-    axios.defaults.baseURL = BASE_URL;
+    axios.defaults.baseURL = 'http://localhost:3003/';
     axios.interceptors.request.use((config) => {
       return config;
     }, (error) => {
@@ -39,31 +39,3 @@ export default new HttpService();
 
 
 
-// import axios, {AxiosRequestConfig} from "axios";
-// import { BASE_URL } from "../utils/variables.config";
-// class HttpService {
-// constructor() {
-// axios.defaults.baseURL = BASE_URL;
-// axios.interceptors.request.use((config) => {
-// return config;
-// }, (error) => {
-// return Promise.reject(error);
-// })
-// }
-// get(url: string, config: AxiosRequestConfig) {
-// return axios.get(url, config);
-// }
-// post(url: string, data: any, config: AxiosRequestConfig) {
-// return axios.post(url, data, config);
-// }
-// put(url: string, data: any, config: AxiosRequestConfig) {
-// return axios.put(url, data, config);
-// }
-// patch(url: string, data: any, config: AxiosRequestConfig) {
-// return axios.patch(url, data, config);
-// }
-// delete(url: string, config: AxiosRequestConfig) {
-// return axios.delete(url, config);
-// }
-// }
-// export default new HttpService();

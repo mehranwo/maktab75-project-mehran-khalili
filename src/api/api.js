78ca7869b373cfbf43ebdba26c33058dbec014b3
mehran <me.dr7725@gmail.com>
2022-08-12 12:28:08 +0430

    update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,22 @@
+import { Filter2 } from '@mui/icons-material';
 import HttpService from '../services/index'
 
 
-export const getPaginatedData =async ({page ,limit , field})=>{
-    const res = await HttpService.get(`${field}?_page=${page}&?_limit=${limit}`)
-    return { data:res.data, count:res.headers["x-total-count"] };     
+
+export const getPaginatedData =async (page ,limit , field)=>{
+    const res = await HttpService.get(`${field}?_page=${page}&_limit=${limit}`)
+    const result = { products:res.data, count:res.headers["x-total-count"] }
+    return result ;     
 }
 
-export const getData =async ({field})=>{
+export const getData =async (field)=>{
     const res = await HttpService.get(field)
     return res.data   
+}
+
+
+export const getFilteredData =async ( field , filter)=>{
+    const res = await HttpService.get(`${field}?${filter}`)
+    const result = { products:res.data, count:res.headers["x-total-count"] }
+    return result ;     
 }

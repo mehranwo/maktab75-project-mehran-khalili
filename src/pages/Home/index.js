cecb4a22d2b79e42ca45ff66df40a62b493391aa
mehran <me.dr7725@gmail.com>
2022-08-12 12:28:41 +0430

    update src/pages/Home/index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,7 +23,6 @@ const Home = () => {
           <Appbar />
           <Banner />
           <Promotions />
-          <ManagmentProducts />
         </Container>
       </RTL>
     </ThemeProvider>

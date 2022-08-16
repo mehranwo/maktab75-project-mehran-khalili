742d6fea37f57e1e1780bd2904fb4bd18b94b3ad
mehran <me.dr7725@gmail.com>
2022-08-16 10:46:30 +0430

    modify routing and link appbar

--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -1,37 +1,34 @@
-import { ImageListItem, ListItemButton, ListItemIcon} from "@mui/material";
+import { ImageListItem, ListItemButton, ListItemIcon } from "@mui/material";
 import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
 import SearchIcon from "@mui/icons-material/Search";
-import * as React from 'react';
+import * as React from "react";
 import Actions from "./actions";
-import image1 from "../../asset/the-hanger.jpg"
+import image1 from "../../asset/the-hanger.jpg";
 import ListItemLink from "../ListItemLink";
 import { Link } from "react-router-dom";
 export default function AppbarDesktop({ matches }) {
   return (
     <AppbarContainer>
-      <ImageListItem>
-        <img src={image1} />
-      </ImageListItem>
+      <Link to="/">
+        <ImageListItem>
+          <img src={image1} />
+        </ImageListItem>
+      </Link>
       <AppbarHeader>کمد</AppbarHeader>
       <MyList type="row">
-        <ListItemLink to="/Home" primary="صفحه اصلی"  />
-        <ListItemLink to="/" primary="دسته بندی ها" />
-        <ListItemLink to="/" primary="محصولات" />
-        <ListItemLink to="/" primary="ارتباط با ما" />
-        <ListItemButton sx={{ flexGrow:0 , display:"flex",justifyContent:"center" }}>
-          <ListItemIcon sx={{ display:"flex",justifyContent:"center" }}>
+        <ListItemLink to="/" primary="صفحه اصلی" />
+        <ListItemLink to="/categoreis" primary="دسته بندی ها" />
+        <ListItemLink to="/products" primary="محصولات" />
+        <ListItemLink to="/contact" primary="ارتباط با ما" />
+        <ListItemButton
+          sx={{ flexGrow: 0, display: "flex", justifyContent: "center" }}
+        >
+          <ListItemIcon sx={{ display: "flex", justifyContent: "center" }}>
             <SearchIcon />
           </ListItemIcon>
         </ListItemButton>
       </MyList>
-      <Actions matches={matches}/>
+      <Actions matches={matches} />
     </AppbarContainer>
   );
 }
-
-
-
-
-
-
-

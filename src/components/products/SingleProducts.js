0ad9aead70e5a81d06194ac1951e1a1b02ebc93d
mehran <me.dr7725@gmail.com>
2022-08-16 10:48:54 +0430

    feat: create SingleProducts.js

---
+++ src/components/products/SingleProducts.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import {
+  ExtraActionsWrapper,
+  Product,
+  ProductActionButton,
+  ProductActionsWrapper,
+  ProductAddToCart,
+  ProductFavButton,
+  ProductImage,
+  ProductMetaWrapper,
+} from "../../styles/product";
+import { Stack, Tooltip, Typography } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import FitScreenIcon from "@mui/icons-material/FitScreen";
+import useDialogModal from "../../hooks/useDialogModal";
+import ProductDetail from "../productdetail";
+import ProductMeta from "./ProductMeta";
+
+export default function SingleProduct({ product, matches }) {
+  const [ProductDetailDialog, showProductDetailDialog, closeProductDialog] =
+    useDialogModal(ProductDetail);
+
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowOptions(true);
+  };
+  const handleMouseLeave = () => {
+    setShowOptions(false);
+  };
+  return (
+    <>
+      <Product onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <ProductImage sx={{"&:hover" : {backgroundImage:`url(http://localhost:3003/files/${product.src[1]})`}}}>
+            <img src={`http://localhost:3003/files/${product.src[0]}`} width={'100%'} height={'100%'}/>
+        </ProductImage>
+        <ProductMeta product={product} matches={matches} />
+        <ProductActionsWrapper>
+          <Stack direction={matches ? "row" : "column"}>
+            <ProductFavButton isfav={0}>
+              <FavoriteIcon />
+            </ProductFavButton>
+            <ProductActionButton>
+              <Tooltip placement="left" title="share this product">
+                <ShareIcon color="primary" />
+              </Tooltip>
+            </ProductActionButton>
+            <ProductActionButton onClick={() => showProductDetailDialog()}>
+              <Tooltip placement="left" title="Full view">
+                <FitScreenIcon color="primary" />
+              </Tooltip>
+            </ProductActionButton>
+          </Stack>
+        </ProductActionsWrapper>
+      </Product>
+      <ProductAddToCart variant="contained">Add to cart</ProductAddToCart>
+      <ProductDetailDialog product={product} />
+    </>
+  );
+}

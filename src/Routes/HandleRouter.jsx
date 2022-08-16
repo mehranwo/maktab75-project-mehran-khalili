ffdfa9579738d49dd8a49d99a3a26ef0e4473088
mehran <me.dr7725@gmail.com>
2022-08-16 10:52:23 +0430

    update HandleRouter.jsx

--- src/Routes/HandleRouter.jsx
+++ src/Routes/HandleRouter.jsx
@@ -8,6 +8,10 @@ import Login from '../pages/Login/LoginPage'
 import { ManagmentProducts } from '../pages/managment'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Contact from '../pages/Contact/Contact'
+import BuyPage from '../pages/BuyPage/BuyPage'
+import Favorite from '../pages/Favorite/Favorite'
+import  ProductShow  from '../pages/product/Product'
 
 export default function HandleRouter() {
   return (
@@ -15,8 +19,12 @@ export default function HandleRouter() {
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>} />
         <Route path='/admin' element={<ManagmentProducts/>}/>
-        {/* 
+        <Route path= '/products' element={<></>}/>
+        <Route path='/contact' element={<Contact/>}/>
         <Route path='/buyPage' element={<BuyPage/>} />
+        <Route path='/Favorite' element={<Favorite/>} />
+        <Route path='/shop/:id' element={<ProductShow/>}/>
+        {/* 
         <Route path='/finalBuy' element={<FinalBuy/>} />
         <Route path='/resultBuy' element={<ResultBuy/>} />
         <Route path='/adminDoshboard' element={<AdminDoshboard/>} />

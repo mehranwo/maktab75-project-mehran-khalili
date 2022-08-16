af50e03f99f486924a1267f7ff780975e3eec029
mehran <me.dr7725@gmail.com>
2022-08-16 10:47:19 +0430

    modify authentication

--- src/components/loginForm/Login.js
+++ src/components/loginForm/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Paper,
@@ -9,13 +9,59 @@ import {
   Link,
   Box,
 } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { FormControlLabel } from "@mui/material";
 import { Checkbox } from "@mui/material";
 import { Colors } from "../../styles/theme";
-import ListItemLink from "../ListItemLink";
+import axios from "axios";
+import HttpService from "../../services/index";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  // toastify
+  const notifyFasle = () => toast.error("نام کاربری یا رمز عبور اشتباه است" , {
+    position: "bottom-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+  const notifyTrue = () => toast.success("خوش آمدید" , {
+    position: "bottom-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
+  const authentication = async (obj) => {
+    const response = await axios.post("http://localhost:3003/auth/login", obj)
+      .then((res)=>{
+        console.log(res.data);
+        localStorage.setItem("TOKEN", res.data.token);
+        setTimeout(()=>{
+          navigate("/admin");
+        },1000)
+        notifyTrue()
+      })
+      .catch((err)=>{
+        localStorage.clear();
+        notifyFasle()
+      })
+  };
+
   const paperStyle = {
     padding: 20,
     height: "70vh",
@@ -43,18 +89,33 @@ const Login = () => {
           justifyContent={"space-between"}
           alignItems={"flex-start"}
         >
-          <TextField label="نام کاربری" fullWidth required />
-          <TextField label="رمز عبور" type="password" fullWidth required />
+          <TextField
+            label="نام کاربری"
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            fullWidth
+            required
+          />
+          <TextField
+            label="رمز عبور"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            fullWidth
+            required
+          />
           <Button
             type="submit"
             color="primary"
             variant="contained"
             style={btnstyle}
             fullWidth
+            onClick={() => {
+              authentication(user);
+            }}
           >
-            <ListItemLink primary="ورود" to="/admin" />
+            ورود
           </Button>
-
           <FormControlLabel
             dir="ltr"
             label="مرا به خاطرت نگهدار"
@@ -74,6 +135,17 @@ const Login = () => {
               ثبت نام
             </Link>
           </Typography>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
         </Box>
       </Paper>
     </Grid>

a319901145a6207142d4a0b7e1cfd29d5d661f07
mehran <me.dr7725@gmail.com>
2022-08-19 17:07:15 +0430

    update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -20,3 +20,18 @@ export const getFilteredData =async ( field , filter)=>{
     const result = { products:res.data, count:res.headers["x-total-count"] }
     return result ;     
 }
+
+export const deleteData = async (field , id)=>{
+    const res = await HttpService.delete(`${field}/${id}`)
+    return res.data
+}
+
+export const updateData = async(field , id , obj)=>{
+    const res = await HttpService.patch(`${field}/${id}`,obj)
+    return res.data
+}
+export const postData = async(field , obj)=>{
+    const res = await HttpService.post(`${field}`,obj)
+    return res.data
+}
+

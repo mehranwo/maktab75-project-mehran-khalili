5e100e3945d5049591c3a5c13789fefae958a1ba
mehran <me.dr7725@gmail.com>
2022-08-19 17:10:45 +0430

    update src/redux/reducers/index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
-import { productsReducer, selectedProductsReducer } from "./productsReducer";
+import { selectedDeletedProduct, productsReducer, selectedProductsReducer, editedProductReducer, addProductReducer } from "./productsReducer";
 const reducers = combineReducers({
   allProducts: productsReducer,
   product: selectedProductsReducer,
+  deletedProduct:selectedDeletedProduct,
+  editedProduct:editedProductReducer,
+  addProduct:addProductReducer,
 });
 export default reducers;

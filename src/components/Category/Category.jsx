5bb4588ee0f354ed8b88ec814875afef80f73604
mehran <me.dr7725@gmail.com>
2022-08-21 04:03:15 +0430

    update Category.jsx

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,43 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+// import "./styles.css";
+import { Pagination, Autoplay ,Navigation } from "swiper";
+import SingleProductDesktop from "../products/SingleProductDesktop";
+import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+
+export default function Category({ type, products }) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box>
+      <Typography variant="h5" display={"flex"} alignItems={"center"}>
+        {type}
+        <ArrowBackIosRoundedIcon fontSize="small" />
+      </Typography>
+      <Box>
+        <Swiper
+          slidesPerView={5}
+          spaceBetween={30}
+          slidesPerGroup={4}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+
+          modules={[Pagination , Autoplay , Navigation  ]}
+          className="mySwiper"
+        >
+          {products.length > 0 &&
+            products.map((product) => (
+              <SwiperSlide>
+                {<SingleProductDesktop product={product} matches={matches} />}
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </Box>
+    </Box>
+  );
+}

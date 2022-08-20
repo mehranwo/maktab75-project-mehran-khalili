235d1501dcb68ebfdd84a9d5896c6d906ac42b76
mehran <me.dr7725@gmail.com>
2022-08-21 04:03:47 +0430

    update ProductMeta.jsx

--- src/components/products/ProductMeta.jsx
+++ src/components/products/ProductMeta.jsx
@@ -3,7 +3,7 @@ import { ProductMetaWrapper } from "../../styles/product";
 export default function ProductMeta({ product, matches }) {
     return (
       <ProductMetaWrapper>
-        <Typography variant={matches ? "h6" : "h5"} lineHeight={2}>
+        <Typography variant={"h6"} lineHeight={2}>
           {product.productName}
         </Typography>
         <Typography variant={matches ? "caption" : "body1"}>

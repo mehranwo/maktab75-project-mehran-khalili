48e1bd84057b357fe6a7155afb9f60d9367523d7
mehran <me.dr7725@gmail.com>
2022-08-26 13:22:13 +0430

    update LoginPage.jsx

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import Appbar from '../../components/appbar'
-import Login from '../../components/loginForm/Login'
+import React from "react";
+import Appbar from "../../components/appbar";
+import Login from "../../components/loginForm/Login";
 import { ThemeProvider } from "@mui/system";
 import { Button, Container } from "@mui/material";
 import theme from "../../styles/theme";
-import RTL from '../../rtl';
+import RTL from "../../rtl";
 
 export default function LoginPage() {
   return (
     <ThemeProvider theme={theme}>
-        <RTL>
+      <RTL>
         <Container
           maxWidth="xl"
           sx={{
-            background:"#fff"
+            width: "100vw",
+            height: "102vh",
+            background: "#fff",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundImage: "url('./images/login/1.jpg')",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
           }}
         >
-          <Appbar />
-          <Login/>
+          <Login />
         </Container>
-        </RTL>
-      </ThemeProvider>
-  )
+      </RTL>
+    </ThemeProvider>
+  );
 }
